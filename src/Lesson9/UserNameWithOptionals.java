792146303f160e8a0e625f01972522d44dbfcf20
maciek-xyz <maciekrmcf@gmail.com>
2021-01-31 16:15:20 +0100

    UserName - correct

--- src/Lesson9/UserNameWithOptionals.java
+++ src/Lesson9/UserNameWithOptionals.java
@@ -1,25 +1,14 @@
 package Lesson9;
 
 import java.time.LocalDate;
-import java.util.Date;
 
 public class UserNameWithOptionals {
     public static void main(String[] args) {
-
-        UserName userName1 = new UserName("Jan", "Kowalski", LocalDate.of(1990,03,29));
-
+        UserName userName1 = new UserName("Jan", "Kowalski", null);
         String firstLetter = generateUserName(userName1.getName(), x -> String.valueOf(x.charAt(0))).toLowerCase();
         String secondName = generateUserName(userName1.getSecondName(), x -> x.toLowerCase());
-        int year = generateBirthday(userName1.getDate(), x -> {
-            if (x == null) {
-                return 2021;
-            } else {
-                return x.getYear();
-            }
-                });
-
-
-        System.out.println("UserName: " + firstLetter + secondName + year);
+        int year = generateBirthday(userName1.getDate(),x -> x.getYear());
+        System.out.println("Username: " + firstLetter + secondName + year);
     }
 
     static String generateUserName(String string, GenerateUserName generateUserName) {
@@ -27,8 +16,12 @@ public class UserNameWithOptionals {
     }
 
     static int generateBirthday(LocalDate year, GenerateBirthday generateBirthday){
-        return generateBirthday.doSomething(year);
-
+        if (year == null) {
+            return LocalDate.now().getYear();
+        } else {
+           return year.getYear();
+        }
     }
+
 
 }
